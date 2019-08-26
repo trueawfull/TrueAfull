14e68b5a960d86be846fc8fdfa102a4e65798166
ICSstar <36115868+ICSstar@users.noreply.github.com>
2019-08-26 19:23:17 -0400

    Gun and Mech scriptable objects
    Co-Authored-By: truesephah <truesephah@users.noreply.github.com>

---
+++ TrueAwfull/Assets/Gun.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Gun", menuName = "Mechs/GunData", order = 1)]
+public class Gun : ScriptableObject
+{
+    public string name;
+    public Transform model;
+    public int magSize;
+    public float damage;
+}
